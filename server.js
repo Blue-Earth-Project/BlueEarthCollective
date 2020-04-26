3388aa60733c4b93d8b89c8fa97fe2ba2eb2dab3
Rishabh Gupta <belikerishabh@gmail.com>
2020-04-26 19:07:19 +0530

    deploy try 2

--- server.js
+++
@@ -1,8 +0,0 @@
-//Backend Code goes in here.
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.listen('/', () => {
-    console.log(`Server is running on ${PORT} port`);
-})
